87f9d21c587f78ae12ce41e4e252eb5da2749d2c
Jonavoe <voeffray.jonathan@gmail.com>
2023-05-08 18:03:15 -0300

    Tailwind y componentes creados

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-	return (
-		<div >
-			<h1 className='text-xl' >SMLAPP</h1>
-		</div>
-	);
+	return <div className=''></div>;
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-		'./src/**/*.{js,ts,jsx,tsx}',
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('tailwind-scrollbar')],
-};
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
