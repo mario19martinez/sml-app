d25a01355a56b9594c1d6c58a9a9d51d93677c68
Nikitoo17 <nicolasfadel17@gmail.com>
2023-05-08 20:30:30 -0300

    fixed tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <div className=" bg-red-600">
+      <App />
+    </div>
   </BrowserRouter>
 );
--- client/src/views/Employees/Employees.jsx
+++ client/src/views/Employees/Employees.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import style from "./Employees.module.css"
+import style from "./Employees.module.css";
 
 function Employees() {
   return (
-  <div className={style.container}>
-    <h1>Employees</h1>
-  </div>
+    <div className={style.container}>
+      <h1 className="  text-white">Employees</h1>
+    </div>
   );
 }
 
