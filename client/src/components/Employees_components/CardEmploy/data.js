9d0711e10390861f88b46e7d025c8ecb90cbae15
juanfacundotam <facutam@gmail.com>
2023-05-09 11:12:09 -0300

    actualizacion estilos
    actualización de mínimos cambios

--- client/src/components/Employees_components/CardEmploy/data.js
+++ client/src/components/Employees_components/CardEmploy/data.js
@@ -3,7 +3,7 @@ let data = [
     image: "https://i.pravatar.cc/150",
     name: "Tomas Agilar",
     phone: "11-5566-8899",
-    position: "corredor",
+    position: "Corredor",
     color: "green",
     extra: "...",
   },
