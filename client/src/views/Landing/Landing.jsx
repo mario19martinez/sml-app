b5d308e791783b298c7ada11d7aa247960755c89
Nikitoo17 <nicolasfadel17@gmail.com>
2023-05-09 13:04:48 -0300

    merge

---
+++ client/src/views/Landing/Landing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "./Landing.module.css";
+import { Link } from "react-router-dom";
+
+function Employees() {
+  return (
+    <div className={style.container}>
+      <h1>SML VIEWS</h1>
+      <Link to="/employees" className={style.linksRoutes}>
+        Employees
+      </Link>
+      <Link to="/analytics" className={style.linksRoutes}>
+        Analytics
+      </Link>
+      <Link to="/login" className={style.linksRoutes}>
+        Login
+      </Link>
+    </div>
+  );
+}
+
+export default Employees;
