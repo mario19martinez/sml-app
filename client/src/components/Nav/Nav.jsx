e8fc5365bc6487d5ff58dfc97fa136a371f99387
Nikitoo17 <nicolasfadel17@gmail.com>
2023-05-09 13:01:31 -0300

    update APP

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,68 +1,69 @@
-import React from 'react';
-import style from './Nav.module.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import style from "./Nav.module.css";
+import { Link } from "react-router-dom";
 import {
-	IoLogOut,
-	IoStatsChart,
-	IoSettingsSharp,
-	IoGrid,
-} from 'react-icons/io5';
+  IoLogOut,
+  IoStatsChart,
+  IoSettingsSharp,
+  IoGrid,
+} from "react-icons/io5";
 
 function Nav() {
-	return (
-		<div className={style.container}>
-			<div className={style.containerContent}>
-				<div className={style.contentTitle}>
-					<div className={style.title}>
-						<Link to='/'>
-							<span>ICONO</span>
-						</Link>
-						<h1>SML</h1>
-					</div>
-					<div />
-					<div className={style.contentList}>
-						<div className={style.list}>
-							<ul>
-								<li>
-									<span>
-										<IoGrid />
-									</span>
-									<span>Dashboard</span>
-								</li>
-								<li>
-									<span>
-										<IoStatsChart />
-									</span>
-									<span>Analytics</span>
-								</li>
-								<li>
-									<span>
-										<IoSettingsSharp />
-									</span>
-									<span>Settings</span>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
+  return (
+    <div className={style.container}>
+      <div className={style.containerContent}>
+        <div className={style.contentTitle}>
+          <div className={style.title}>
+            <Link to="/">
+              <span>ICONO</span>
+            </Link>
+            <h1>SML</h1>
+          </div>
+          <div />
+          <div className={style.contentList}>
+            <div className={style.list}>
+              <ul>
+                <li>
+                  <span>
+                    <IoGrid />
+                  </span>
+                  <span>Dashboard</span>
+                </li>
+                <li>
+                  <span>
+                    <IoStatsChart />
+                  </span>
+                  <span>Analytics</span>
+                </li>
+                <li>
+                  <span>
+                    <IoSettingsSharp />
+                  </span>
+                  <span>Settings</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
-				<div className={style.log}>
-					<img
-						className={style.img}
-						src={
-							'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFADjIwjVTPKIf_2YkWdv2-N5TPzIItNC0TQ&usqp=CAU'
-						}></img>
-					<div className={style.text}>
-						<p>Easin Arafat</p>
-						<p>Free Account</p>
-					</div>
-					<div className={style.logout}>
-						<IoLogOut />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className={style.log}>
+          <img
+            className={style.img}
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFADjIwjVTPKIf_2YkWdv2-N5TPzIItNC0TQ&usqp=CAU"
+            }
+          ></img>
+          <div className={style.text}>
+            <p>Easin Arafat</p>
+            <p>Free Account</p>
+          </div>
+          <div className={style.logout}>
+            <IoLogOut />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import style from "./Landing.module.css"
-import { Link } from "react-router-dom"
+import style from "./Landing.module.css";
+import { Link } from "react-router-dom";
 
 function Employees() {
   return (
-  <div className={style.container}>
-    <h1>SML VIEWS</h1>
-    <Link to="/employees" className={style.linksRoutes}>Employees</Link>
-    <Link to="/analytics" className={style.linksRoutes}>Analytics</Link>
-    <Link to="/login" className={style.linksRoutes}>Login</Link>
-
-  </div>
+    <div className={style.container}>
+      <h1>SML VIEWS</h1>
+      <Link to="/employees" className={style.linksRoutes}>
+        Employees
+      </Link>
+      <Link to="/analytics" className={style.linksRoutes}>
+        Analytics
+      </Link>
+      <Link to="/login" className={style.linksRoutes}>
+        Login
+      </Link>
+    </div>
   );
 }
 
