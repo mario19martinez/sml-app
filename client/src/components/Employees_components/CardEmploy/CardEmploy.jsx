b1d072ebd22cc2e5a7ed438e8622a84967728700
juanfacundotam <facutam@gmail.com>
2023-05-09 10:26:42 -0300

    CardEmployees map
    Realice el mapeo con una data hardcodeada

--- client/src/components/Employees_components/CardEmploy/CardEmploy.jsx
+++ client/src/components/Employees_components/CardEmploy/CardEmploy.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import style from "./CardEmploy.module.css";
+import data from "./data";
 
 function CardEmploy() {
   return (
     <div className={style.divCard}>
-      <h6>#999999</h6>
-
-      <h6>#999999</h6>
-
-      <h6>#999999</h6>
-
-      <h6>#999999</h6>
-
+      {data.map((char) => {
+        return (<ul className={style.list}>
+          <li className={style.liImage}>
+            <img src={char.image} alt="" />
+          </li>
+          <li className={style.liName}>{char.name}</li>
+          <li className={style.liPhone}>{char.phone}</li>
+          <li className={style.liPosition}>{char.position}</li>
+          <li className={style.liExtra}>{char.extra}</li>
+        </ul>);
+      })}
     </div>
   );
 }
