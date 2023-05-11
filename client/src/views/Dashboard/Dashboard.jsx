d1843eab2f0b758c4cd4e1282092f64f28aa65fa
Jonavoe <voeffray.jonathan@gmail.com>
2023-05-11 18:24:53 -0300

    Cambios al Dashboard

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import Nav from "../../components/Nav/Nav";
-import { CardPotencialLead } from "../../components/Vendedores/Dashboard/CardPotencialLead";
+import React from 'react';
+import Nav from '../../components/Nav/Nav';
+import DashboardVendedores from '../../components/Vendedores/Dashboard/CardPotencialLead';
 
 function Dashboard() {
-  return (
-    <div className="flex flex-row w-screen">
-      <Nav />
-      <div className="">
-        <div>Dashboard</div>
-        <CardPotencialLead />
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-row w-screen'>
+			<Nav />
+			<div className=''>
+				<div>Dashboard</div>
+				<DashboardVendedores />
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
