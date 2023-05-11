509369963a40ed9606f397912c60bf207cf50668
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-05-11 17:37:50 -0300

    Update Dashboard.jsx

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../../components/Nav/Nav";
-import { CardPotencialLead } from "../../components/Vendedores/Dashboard/CardPotencialLead";
+import { CardPotencialLead } from "../../components/Vendedores/Dashboard/VendedoresDashboard";
 
 function Dashboard() {
   return (
