7b9fa1978e89ee4a54a122d01e1d4a23589c1259
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-05-12 04:17:29 -0300

    ahora si a dormir

--- client/src/components/Vendedores/analytics/VendedoresAnalytics.jsx
+++ client/src/components/Vendedores/analytics/VendedoresAnalytics.jsx
@@ -27,7 +27,7 @@ const AnalyticsSealer = () => {
       nivel: 0,
       telefono: '+222222222',
       Email: 'siemprepar@gmail.com',
-      status: false
+      status: true
     },
     {
       id: '242562',
@@ -54,7 +54,7 @@ const AnalyticsSealer = () => {
       nivel: 0,
       telefono: '+222222222',
       Email: 'siemprepar@gmail.com',
-      status: false
+      status: true
     },
     {
       id: '222222',
@@ -63,7 +63,7 @@ const AnalyticsSealer = () => {
       nivel: 0,
       telefono: '+222222222',
       Email: 'siemprepar@gmail.com',
-      status: false
+      status: true
     },
     {
       id: '244444',
@@ -72,7 +72,7 @@ const AnalyticsSealer = () => {
       nivel: 0,
       telefono: '+222222222',
       Email: 'siemprepar@gmail.com',
-      status: false
+      status: true
     },
     {
       id: '255555',
@@ -90,7 +90,7 @@ const AnalyticsSealer = () => {
       nivel: 0,
       telefono: '+222222222',
       Email: 'siemprepar@gmail.com',
-      status: false
+      status: true
     },
     {
       id: '277777',
@@ -119,46 +119,134 @@ const AnalyticsSealer = () => {
       Email: 'siemprepar@gmail.com',
       status: false
     },
+    {
+      id: '888888',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: false
+    },
+    {
+      id: '777777',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: false
+    },
+    {
+      id: '666666',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: false
+    },
+    {
+      id: '555555',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: false
+    },
+    {
+      id: '444444',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: false
+    },
+    {
+      id: '333333',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: false
+    },
+    {
+      id: '111111',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: false
+    },
+    {
+      id: '123213',
+      client: 'juenito jobs',
+      profesion: 'macdonalero',
+      nivel: 0,
+      telefono: '+222222222',
+      Email: 'siemprepar@gmail.com',
+      status: true
+    },
   ]
- 
-  const cardsPerPage = 8;
-  const totalPages = Math.ceil(data.length / cardsPerPage);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [currentCards, setCurrentCards] = useState([]);
+  const cardsPerPage = 8
+  const totalPages = Math.ceil(data.length / cardsPerPage)
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [currentCards, setCurrentCards] = useState([])
 
   useEffect(() => {
-    const indexOfLastCard = currentPage * cardsPerPage;
-    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-    const cardsToDisplay = data.slice(indexOfFirstCard, indexOfLastCard);
-    setCurrentCards(cardsToDisplay);
-  }, [currentPage]);
+    const indexOfLastCard = currentPage * cardsPerPage
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage
+    const cardsToDisplay = data.slice(indexOfFirstCard, indexOfLastCard)
+    setCurrentCards(cardsToDisplay)
+  }, [currentPage])
 
 
   const handlePageChange = (page) => {
-    setCurrentPage(page);
+    setCurrentPage(page)
   }
   return (
     <div className="flex w-screen">
       <Nav />
       <div className="flex flex-col w-screen">
-        <div className="flex justify-between m-8">
-          <h1 className="text-2xl font-bold text-white">Analytics Sealers</h1>
-          <select className="w-32 h-12 rounded-lg bg-purple-500 text-white text-center">
+        <div className="flex items-center justify-between m-8">
+          <h1 className="text-2xl font-bold text-white">Analytics Selers</h1>
+          <select className="w-32 h-10 rounded-lg bg-purple-500 text-white text-center">
             <option className="py-1">2023</option>
           </select>
         </div>
         <div className="h-3/5">
           <div className="flex flex-col">
+          <div className="w-full flex justify-center  mb-[30px] ">
+            <div className="flex flex-row  w-[1500px] items-start mt-80px  space-x-52">
+              <div className=" w-1/7 text-center">Invoice ID</div>
+              <div className="w-1/7 text-center">Name</div>
+              <div className=" w-1/7 text-center">Profession</div>
+              <div className="w-1/7 text-center">Nivel</div>
+              <div className="w-1/7 text-center">Email</div>
+              <div className="w-1/7 text-center">Status</div>
+            </div>
+            </div>
             {currentCards.map((item) => (
-              <div className="w-full flex justify-center mb-10 h-3/5" key={item.id}>
+              <div className="w-full flex justify-center mb-8 h-3/5" key={item.id}>
                 <div className="flex flex-row rounded bg-[#39394B] w-[1500px] h-16 items-center mt-80px justify-center space-x-28">
                   <div className="w-1/7 text-center">{item.id}</div>
                   <div className="w-1/7 text-center">{item.client}</div>
                   <div className="w-1/7 text-center">{item.profesion}</div>
                   <div className="w-1/7 text-center">{item.nivel}</div>
+                  {
+                    //falta logica para poner imagen de nivel segun el nivel
+                  }
                   <div className="w-1/7 text-center">{item.telefono}</div>
-                  <div className="w-1/7 text-center">{item.Email}</div>
+                  <div className="w-1/7 text-center">{item.Email}</div> 
+                  {
+                    //falta  poner imagen de mail
+                  }
                   <div className="w-1/7 text-center">
                     {item.status === true ? (
                       <div className="bg-emerald-400 w-44 h-11 flex justify-center items-center text-white rounded-3xl">
@@ -174,24 +262,23 @@ const AnalyticsSealer = () => {
               </div>
             ))}
           </div>
-          <div className="flex justify-center mt-4">
-          {Array.from({ length: totalPages }, (_, index) => (
-            <button
-              key={index}
-              className={`mx-1 px-4 py-2 rounded-lg ${
-                currentPage === index + 1 ? 'bg-purple-500 text-white' : 'bg-gray-200 text-gray-600'
-              }`}
-              onClick={() => handlePageChange(index + 1)}
-            >
-              {index + 1}
-            </button>
-          ))}
+          <div className="flex justify-center mt-6">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index}
+                className={`mx-1 px-4 py-2 rounded-lg ${currentPage === index + 1 ? 'bg-purple-500 text-white' : 'bg-[#39394B] text-white'
+                  }`}
+                onClick={() => handlePageChange(index + 1)}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
         </div>
-        </div>
-       
+
       </div>
     </div>
   )
 }
 
-export default AnalyticsSealer;
+export default AnalyticsSealer
