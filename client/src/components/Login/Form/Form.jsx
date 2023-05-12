0ffaecb4df046f88ee34b9505a8b7b18f8b2cfbe
Jonavoe <voeffray.jonathan@gmail.com>
2023-05-12 11:30:02 -0300

    se saca el Link

--- client/src/components/Login/Form/Form.jsx
+++ client/src/components/Login/Form/Form.jsx
@@ -1,80 +1,79 @@
-import React, { useState } from "react";
-import { validate } from "./validation";
+import React, { useState } from 'react';
+import { validate } from './validation';
 
 const Form = (props) => {
-  const [userData, setUserData] = useState({
-    username: "",
-    password: "",
-  });
+	const [userData, setUserData] = useState({
+		username: '',
+		password: '',
+	});
 
-  const [errors, setErrors] = useState({
-    username: "",
-    password: "",
-  });
+	const [errors, setErrors] = useState({
+		username: '',
+		password: '',
+	});
 
-  const handleInputChange = (event) => {
-    setUserData({
-      ...userData,
-      [event.target.name]: event.target.value,
-    });
+	const handleInputChange = (event) => {
+		setUserData({
+			...userData,
+			[event.target.name]: event.target.value,
+		});
 
-    setErrors(
-      validate({
-        ...userData,
-        [event.target.name]: event.target.value,
-      })
-    );
-  };
+		setErrors(
+			validate({
+				...userData,
+				[event.target.name]: event.target.value,
+			})
+		);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    // Validar si existen errores en los datos
-    const propsArray = Object.values(errors);
-    if (propsArray.some((error) => error !== "")) {
-      alert("Debe llenar todos los campos correctamente");
-      return; // Salir de la función si hay errores
-    }
+		// Validar si existen errores en los datos
+		const propsArray = Object.values(errors);
+		if (propsArray.some((error) => error !== '')) {
+			alert('Debe llenar todos los campos correctamente');
+			return; // Salir de la función si hay errores
+		}
 
-    // Si no hay errores, ejecutar la función de autenticación
-    if (props.Login) {
-      props.Login(userData);
-    }
-    
-    
-    setErrors({
-      username: "",
-      password: "",
-    });
-    setUserData({
-      username: "",
-      password: "",
-    });
-  };
+		// Si no hay errores, ejecutar la función de autenticación
+		if (props.Login) {
+			props.Login(userData);
+		}
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label name="username">Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={userData.username}
-          onChange={handleInputChange}
-        />
-        {errors.username !== "" && <p>{errors.username}</p>}
-        <label name="password">Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={userData.password}
-          onChange={handleInputChange}
-        />
-        {errors.password !== "" && <p>{errors.password}</p>}
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+		setErrors({
+			username: '',
+			password: '',
+		});
+		setUserData({
+			username: '',
+			password: '',
+		});
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label name='username'>Username:</label>
+				<input
+					type='text'
+					name='username'
+					value={userData.username}
+					onChange={handleInputChange}
+				/>
+				{errors.username !== '' && <p>{errors.username}</p>}
+				<label name='password'>Password:</label>
+				<input
+					type='password'
+					name='password'
+					value={userData.password}
+					onChange={handleInputChange}
+				/>
+				{errors.password !== '' && <p>{errors.password}</p>}
+				<button type='submit'>Login</button>
+			</form>
+		</div>
+	);
 };
 
 export default Form;
--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,153 +1,47 @@
-// import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
-// function Login() {
-//   const { loginWithRedirect } = useAuth0();
-
-//   const slackWorkspace ="signinwithslack"
-//   const slackClientId= "3867251818310.5252696531139"
-
-//   const handleGoogleLogin = () => {
-//     window.location.href = `https://accounts.google.com/o/oauth2/auth?client_id=${googleClientId}&redirect_uri=${googleRedirectUri}&response_type=code&scope=openid%20profile%20email`;
-//   };
-
-//   const handleMicrosoftLogin = () => {
-//     window.location.href = `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/authorize?client_id=${microsoftClientId}&response_type=code&redirect_uri=${microsoftRedirectUri}&scope=user.read%20openid%20profile`;
-//   };
-
-//   const handleSlackLogin = () => {
-//     window.location.href = `https://${slackWorkspace}.slack.com/oauth/authorize?client_id=${slackClientId}&scope=identity.basic`;
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleGoogleLogin}>Iniciar sesión con Google</button>
-//       <button onClick={handleMicrosoftLogin}>Iniciar sesión con Microsoft</button>
-//       <button onClick={handleSlackLogin}>Iniciar sesión con Slack</button>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
-
-
-
-
-import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-import style from "./Login.module.css";
-import Form from "../../components/Login/Form/Form";
-import { validate } from "../../components/Login/Form/validation";
-import { Link } from "react-router-dom";
-
+import { useAuth0 } from '@auth0/auth0-react';
+import style from './Login.module.css';
+import { validate } from '../../components/Login/Form/validation';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+	const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  const handleFormLogin = async (userData) => {
-    const errors = validate(userData);
-    const hasErrors = Object.values(errors).some((error) => error !== "");
+	const handleFormLogin = async (userData) => {
+		const errors = validate(userData);
+		const hasErrors = Object.values(errors).some((error) => error !== '');
 
-    if (hasErrors) {
-      // Mostrar errores de validación al usuario o realizar alguna acción adicional
-      console.log("Errores de validación:", errors);
-    } else {
-      try {
-        // Lógica para autenticar con el formulario
-        // Realizar la solicitud al servidor para verificar las credenciales del usuario
-        // Por ejemplo, utilizando una API o un servicio de autenticación
+		if (hasErrors) {
+			// Mostrar errores de validación al usuario o realizar alguna acción adicional
+			console.log('Errores de validación:', errors);
+		} else {
+			try {
+				// Lógica para autenticar con el formulario
+				// Realizar la solicitud al servidor para verificar las credenciales del usuario
+				// Por ejemplo, utilizando una API o un servicio de autenticación
 
-        // Si la autenticación es exitosa, redirigir a la ruta "/"
-        window.location.href = "/home";
-      } catch (error) {
-        // Manejo de errores de autenticación
-        console.log(error);
-      }
-    }
-  };
+				// Si la autenticación es exitosa, redirigir a la ruta "/"
+				window.location.href = '/home';
+			} catch (error) {
+				// Manejo de errores de autenticación
+				console.log(error);
+			}
+		}
+	};
 
-  return (
-    <div className={style.container}>
-      {!isAuthenticated }
-      {isAuthenticated && <Link to="/home">Home</Link>}
-      {!isAuthenticated && (
-        <button
-          onClick={() => loginWithRedirect()}
-          className={`${style["auth-button"]} ${style["google-button"]}`}
-        >
-          <span className={style["google-icon"]}></span>
-          <span className={style["button-text"]}>Login</span>
-        </button>
-      )}
-    </div>
-  );
+	return (
+		<div className={style.container}>
+			{!isAuthenticated}
+			{isAuthenticated && <Link to='/home'>Home</Link>}
+			{!isAuthenticated && (
+				<button
+					onClick={() => loginWithRedirect()}
+					className={`${style['auth-button']} ${style['google-button']}`}>
+					<span className={style['google-icon']}></span>
+					<span className={style['button-text']}>Login</span>
+				</button>
+			)}
+		</div>
+	);
 }
 
 export default Login;
-
-
-
-//   return (
-//     <div className={style.container}>
-//       {!isAuthenticated && (
-//         <>
-//           <Form Login={handleFormLogin} />
-//           <h1 className="underline">hola</h1>
-//           <button
-//             onClick={() => loginWithRedirect()}
-//             className={`${style["auth-button"]} ${style["google-button"]}`}
-//           >
-//             <span className={style["google-icon"]}></span>
-//             <span className={style["button-text"]}>Google</span>
-//           </button>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Login;
-
-// {/* <p>Username</p>
-//       <input type="text" placeholder="Title" />
-//       <p>Password</p>
-//       <input type="password" placeholder="Title" />
-//       <label>
-//         <input type="checkbox" /> Remember me
-//       </label>
-//       <p>Forgot your password? <a href="#">Reset Password</a></p>
-//       <button type="submit">Login</button> */}
-
-// import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// function Login() {
-//   const { loginWithRedirect } = useAuth0();
-//   const googleRedirectUri = window.location.origin + "/login/callback"
-  
-
-//   const handleGoogleLogin = () => {
-//     loginWithRedirect({
-//       screen_hint: "signup",
-//       connection: "google-oauth2",
-//       redirect_uri: googleRedirectUri,
-//     });
-//   };
-//   const handleMicrosoftLogin = () => {
-//     window.location.href = `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/authorize?client_id=${microsoftClientId}&response_type=code&redirect_uri=${microsoftRedirectUri}&scope=user.read%20openid%20profile`;
-//   };
-
-//   const handleSlackLogin = () => {
-//     window.location.href = `https://${slackWorkspace}.slack.com/oauth/authorize?client_id=${slackClientId}&scope=identity.basic`;
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleGoogleLogin}>Iniciar sesión con Google</button>
-//       <button onClick={handleMicrosoftLogin}>Iniciar sesión con Microsoft</button>
-//       <button onClick={handleSlackLogin}>Iniciar sesión con Slack</button>
-//     </div>
-//   );
-// }
-
-//  export default Login;
