b4fc832af4e60073e96d44192f1f47e9bbbdd3e9
juanfacundotam <facutam@gmail.com>
2023-05-12 10:43:21 -0300

    Dashboard Corredores integrated
    Se integro la dashboard corredores creada por GustavoPala y Mario

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./views/Landing/Landing";
 import Employees from "./views/Employees/Employees.jsx";
 import Analytics from "./views/Analytics/Analytics.jsx";
-import CorredoresAnalytics from "./components/Corredores/Analitycs/CorredoresAnalytics";
+// import CorredoresAnalytics from "./components/Corredores/Analitycs/CorredoresAnalytics";
 import Settings from "./views/Settings/Settings.jsx";
 import Login from "./views/Login/Login";
 import CorredoresDashboard from "./components/Corredores/Dashboard/CorredoresDashboard"
