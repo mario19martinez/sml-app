05a6308a57f23b6cc625901e09601f5e41c71d05
juanfacundotam <facutam@gmail.com>
2023-05-15 17:16:22 -0300

    cambios estilos

--- client/src/components/Vendedores/Dashboard/VendedoresDashboard.jsx
+++ client/src/components/Vendedores/Dashboard/VendedoresDashboard.jsx
@@ -10,6 +10,7 @@ import { CiEdit, CiWarning, CiInstagram, CiMail } from "react-icons/ci";
 import { FaRegEdit } from "react-icons/fa";
 import { BiEdit } from "react-icons/bi";
 import { AiFillEdit } from "react-icons/ai";
+import { IoGrid, IoStatsChart } from 'react-icons/io5';
 
 import {
   Card,
@@ -46,10 +47,9 @@ const VendedoresDashboard = () => {
     dispatch(getAllLead());
   }, [dispatch]);
 
-
-    
   const handleCopyClick = (copyToProps) => {
-    navigator.clipboard.writeText(copyToProps)
+    navigator.clipboard
+      .writeText(copyToProps)
       .then(() => {
         setShowCopiedMessage(true);
         setTimeout(() => setShowCopiedMessage(false), 2000);
@@ -57,128 +57,143 @@ const VendedoresDashboard = () => {
       .catch((err) => alert(`Error al copiar: ${err}`));
   };
 
-
   return (
     <>
       <Nav />
-      <div className="flex flex-col justify-start items-center w-full h-fit mx-5">
-      {showCopiedMessage && <p className="mt-2 p-3 bg-[#b9b9b978] text-white rounded-md absolute">Copiado al portapapeles</p>}
-        <Card className="w-full m-5 h-screen ">
-          <div className="flex justify-between items-center mt-2 mx-5 ">
-            <Title className="font-bold text-[#e2e2e2] text-lg">
-              Dashboard
-            </Title>
-          </div>
+        <div className="flex flex-col justify-start items-center w-full h-screen mx-5">
+          {/* {showCopiedMessage && <p className="mt-2 p-3 bg-[#b9b9b978] text-white rounded-md absolute">Copiado al portapapeles</p>} */}
+          <Card className="w-full m-5 mx-5">
+            <div className="flex gap-10 items-center mt-2 mx-5 ">
+              <Title className="font-bold text-[#e2e2e2] text-lg">
+                Dashboard
+              </Title>
+              <div className='flex gap-5'>
+							<Link to={'/vendedores'}>
+								<IoGrid className='text-[2rem] text-[#418df0] hover:text-[#3570bd]' />
+							</Link>
+							<Link
+								className='text-5xl'
+								to={'/vendedores/analytics'}>
+								<IoStatsChart className='text-[2rem] text-[#418df0] hover:text-[#3570bd]' />
+							</Link>
+						</div>
+            </div>
 
-          <Table className="flex">
-            <TableHead className="text-gray-400 text-14 font-thin">
-              <TableRow className={style.tableRow}>
-                <TableHeaderCell className="text-start">
-                  Invoice Id
-                </TableHeaderCell>
-                <TableHeaderCell className="text-start">Name</TableHeaderCell>
-                <TableHeaderCell className="text-start">
-                  Profesion
-                </TableHeaderCell>
-                <TableHeaderCell className="text-start">
-                  Country
-                </TableHeaderCell>
-                {/* <TableHeaderCell className="text-start">
+            <Table className="flex">
+              <TableHead className="text-gray-400 text-14 font-thin">
+                <TableRow className={style.tableRow}>
+                  <TableHeaderCell className="text-start">
+                    Invoice Id
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-start">Name</TableHeaderCell>
+                  <TableHeaderCell className="text-start">
+                    Profesion
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-start">
+                    Country
+                  </TableHeaderCell>
+                  {/* <TableHeaderCell className="text-start">
                   Phone
                 </TableHeaderCell> */}
-                <TableHeaderCell className="text-start">Email</TableHeaderCell>
-                <TableHeaderCell className="text-start">
-                  Instagram
-                </TableHeaderCell>
-                <TableHeaderCell className="text-start">Nivel</TableHeaderCell>
-                <TableHeaderCell className="text-start">Status</TableHeaderCell>
-                <TableHeaderCell className="text-start"></TableHeaderCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody className={style.tableBody}>
-              {currentCard.map((item) => (
-                <TableRow key={item._id} className={style.tableCards}>
-                  <TableCell className="flex justify-start items-center p-0">
-                    <div className="w-24 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
-                      {item._id}
-                    </div>
-                  </TableCell>
-                  <TableCell className="flex justify-start items-center  p-0">
-                    <Text className="w-96 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
-                      {item.name}
-                    </Text>
-                  </TableCell>
-                  <TableCell className="flex justify-start items-center p-0">
-                    <Text className="w-96 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">{item.category}</Text>
-                  </TableCell>
-
-                  <TableCell className="flex justify-start items-center p-0">
-                    <Text>{item.province}</Text>
-                  </TableCell>
-
-                  <TableCell className="flex justify-start items-center p-0">
-                    {item.email !== "-" ? (
-                      <div onClick={() => handleCopyClick(item.email)}>
-
-                        <div className="cursor-pointer">
-                          <CiMail className="text-[30px] mr-5 text-[#418df0]" />
-                        </div>
-                      </div>
-                    ) : (
-                      <div>
-                        <CiMail className="text-[30px] mr-5 text-[#9eabbe]" />
-                      </div>
-                    )}
-                  </TableCell>
-                  <TableCell className="flex justify-start items-center p-0 mx-3">
-                    {item.instagram ? (
-                      <div onClick={() => handleCopyClick(item.email)}>
-                        <div className="cursor-pointer">
-                          <CiInstagram className="text-[30px] mr-5 text-[#ff598b]" />
-
-                        </div>
-                      </div>
-                    ) : (
-                      <div>
-                        <CiInstagram className="text-[30px] mr-5 text-[#9eabbe]" />
-
-                      </div>
-                    )}
-                  </TableCell>
-                  <TableCell className="flex justify-start items-center p-0 mx-3">
-                    {item.level !== "incidencia" ? (
-                      <Text className="text-start bg-[#6254ff] text-white py-2 px-3 text-xl rounded-md">
-                        {item.level}
-                      </Text>
-                    ) : (
-                      <Text className="text-start bg-[#6254ff] p-1 text-xl rounded-md text-white">
-                        <CiWarning className="text-white p-0 text-24" />
-                      </Text>
-                    )}
-                  </TableCell>
-                  <TableCell className="flex justify-start items-start p-0">
-                    <button className="bg-[#ff69b4] text-white rounded-3xl px-6 py-1">
-                      Contratado
-                    </button>
-                  </TableCell>
-                  <TableCell className="flex justify-start items-start p-0">
-                  <CiEdit className="bg-[#6254ff] text-1 text-white w-10 h-8 rounded-md cursor-pointer"/>
-                  </TableCell>
+                  <TableHeaderCell className="text-start">
+                    Email
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-start">
+                    Instagram
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-start">
+                    Nivel
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-start">
+                    Status
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-start"></TableHeaderCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Card>
-        <div className="absolute bottom-0">
-          <PaginationOutlined
-            pageStyle={pageStyle}
-            setPageStyle={setPageStyle}
-            cardXPage={cardXPage}
-            data={lead}
-            pages={pages}
-          />
-        </div>
+              </TableHead>
+
+              <TableBody className="h-3/4">
+                {currentCard.map((item) => (
+                  <TableRow key={item._id} className={style.tableCards}>
+                    <TableCell className="flex justify-start items-center p-0">
+                      <div className="w-24 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
+                        {item._id}
+                      </div>
+                    </TableCell>
+                    <TableCell className="flex justify-start items-center  p-0">
+                      <Text className="w-96 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
+                        {item.name}
+                      </Text>
+                    </TableCell>
+                    <TableCell className="flex justify-start items-center p-0">
+                      <Text className="w-96 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
+                        {item.category}
+                      </Text>
+                    </TableCell>
+
+                    <TableCell className="flex justify-start items-center p-0">
+                      <Text>{item.province}</Text>
+                    </TableCell>
+
+                    <TableCell className="flex justify-start items-center p-0">
+                      {item.email !== "-" ? (
+                        <div onClick={() => handleCopyClick(item.email)}>
+                          <div className="cursor-pointer">
+                            <CiMail className="text-[30px] mr-5 text-[#418df0]" />
+                          </div>
+                        </div>
+                      ) : (
+                        <div>
+                          <CiMail className="text-[30px] mr-5 text-[#9eabbe]" />
+                        </div>
+                      )}
+                    </TableCell>
+                    <TableCell className="flex justify-start items-center p-0 mx-3">
+                      {item.instagram ? (
+                        <div onClick={() => handleCopyClick(item.email)}>
+                          <div className="cursor-pointer">
+                            <CiInstagram className="text-[30px] mr-5 text-[#ff598b]" />
+                          </div>
+                        </div>
+                      ) : (
+                        <div>
+                          <CiInstagram className="text-[30px] mr-5 text-[#9eabbe]" />
+                        </div>
+                      )}
+                    </TableCell>
+                    <TableCell className="flex justify-start items-center p-0 mx-3">
+                      {item.level !== "incidencia" ? (
+                        <Text className="text-start bg-[#6254ff] text-white py-2 px-3 text-xl rounded-md">
+                          {item.level}
+                        </Text>
+                      ) : (
+                        <Text className="text-start bg-[#6254ff] p-1 text-xl rounded-md text-white">
+                          <CiWarning className="text-white p-0 text-24" />
+                        </Text>
+                      )}
+                    </TableCell>
+                    <TableCell className="flex justify-start items-start p-0">
+                      <button className="bg-[#ff69b4] text-white rounded-3xl px-6 py-1">
+                        Contratado
+                      </button>
+                    </TableCell>
+                    <TableCell className="flex justify-start items-start p-0">
+                      <CiEdit className="bg-[#6254ff] text-1 text-white w-10 h-8 rounded-md cursor-pointer" />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Card>
+          <div className="mb-5">
+            <PaginationOutlined
+              pageStyle={pageStyle}
+              setPageStyle={setPageStyle}
+              cardXPage={cardXPage}
+              data={lead}
+              pages={pages}
+            />
+          </div>
+
       </div>
     </>
   );
