5f30c2da139a1dd1ae65fa681bc16888bfbb984c
juanfacundotam <facutam@gmail.com>
2023-05-16 21:20:32 -0300

    modal updated
    Se actualizo el modal aun más

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -12,11 +12,11 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "#39394B",
-  border: "2px solid #000",
+  border: "none",
   boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
+  pt: 4,
+  px: 6,
+  pb: 4,
 };
 
 function ChildModal({ setOpen }) {
@@ -25,6 +25,10 @@ function ChildModal({ setOpen }) {
     setOpenChild(true);
   };
   const handleClose = () => {
+    setOpenChild(false);
+
+  };
+  const handleUpdate = () => {
     setOpenChild(false);
     setOpen(false);
     alert("Lead Update");
@@ -36,11 +40,13 @@ function ChildModal({ setOpen }) {
   return (
     <React.Fragment>
       <div className="flex justify-around items-center">
-        <Button
+      <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={handleCancel}>Close x</button>
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleOpen}>Save Changes</button>
+        {/* <Button
           onClick={handleCancel}
           variant="outlined"
           className="text-center bg-black"
-          sx={{ color: 'white', background: '#497d96'}}
+          sx={{ color: "white", background: "#497d96" }}
         >
           close x
         </Button>
@@ -50,7 +56,7 @@ function ChildModal({ setOpen }) {
           className="text-center bg-black"
         >
           Save Changes
-        </Button>
+        </Button> */}
       </div>
       <Modal
         open={openChild}
@@ -58,14 +64,28 @@ function ChildModal({ setOpen }) {
         aria-labelledby="child-modal-title"
         aria-describedby="child-modal-description"
       >
-        <Box sx={{ ...style, width: 300, backgroundColor: "#39394B"}}>
-          <h2 id="child-modal-title" className="text-white">Update Lead!</h2>
-          <p id="child-modal-description" className="text-white">
+        <Box sx={{ ...style, width: 500, backgroundColor: "#39394B", display:"flex", flexDirection:"column", justifyContent:"center", alignContent: "center" }}>
+          <h2 id="child-modal-title" className="text-white text-center">
+            Are you sure?
+          </h2>
+          {/* <p id="child-modal-description" className="text-white">
             ¿Seguro quieres guardar los cambios?
-          </p>
-          <Button onClick={handleClose} sx={{ color: 'white', background: '#497d96'}}>No</Button>
-          <Button onClick={handleClose} variant="contained"
-          className="text-center bg-black">Yes</Button>
+          </p> */}
+          <div className="flex justify-around items-center m-5">
+          <Button
+            onClick={handleClose}
+            sx={{ color: "white", background: "#497d96" }}
+          >
+            No
+          </Button>
+          <Button
+            onClick={handleUpdate}
+            variant="contained"
+            className="text-center bg-black"
+          >
+            Yes
+          </Button>
+          </div>
         </Box>
       </Modal>
     </React.Fragment>
@@ -93,57 +113,84 @@ export default function NestedModal({ item }) {
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: 510 }}>
+        <Box sx={{ ...style, width: 550, borderRadius: 5}}>
           <h2 id="parent-modal-title" className="text-center text-white mb-6">
             {item.name}
           </h2>
-          <div className="flex items-center justify-between mt-10">
-            <div className="flex items-center justify-center relative">
-              <label htmlFor="email" className="mx-5 text-white">
-                Email:
+          <div class="flex flex-col">
+            <div className="mt-2">
+              <label
+                htmlFor="last_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Email
               </label>
-              <div className=" h-10 relative flex items-center justify-start">
-                <label
-                  id="parent-modal-description"
-                  className="text-white text-start w-64 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute"
-                >
-                  {/* {item.email} */}
-                  juanPerezHernandez@estudiolorenzo.com.essss
-                </label>
+              <div className="flex justify-center items-center">
+                <input
+                  type="text"
+                  id="last_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  // placeholder={item.email}
+                  placeholder="google@gmail.com.ar"
+                  required
+                  disabled
+                />
+                <CiEdit
+                  className="bg-[#6254ff] text-1 text-white w-10 h-8 rounded-md cursor-pointer m-2"
+                  onClick={handleOpen}
+                />
               </div>
             </div>
-            <div>
-              <CiEdit
-                className="bg-[#6254ff] text-1 text-white w-10 h-8 rounded-md cursor-pointer text-end"
-                onClick={handleOpen}
-              />
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center justify-center relative">
-              <label htmlFor="email" className="ml-11 mr-5 text-white">
-                Ig:
+            <div className="mt-5">
+              <label
+                htmlFor="last_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Instagram
               </label>
-              <div className="h-10 relative flex items-center justify-start">
-                <label
-                  id="parent-modal-description"
-                  className="text-white text-start w-64 p-1 px-3 rounded-full text-ellipsis opacity-1 overflow-hidden hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute"
-                >
-                  {/* {item.instagram} */}
-                  www.instagram.com/juanPerezHernandez
-                </label>
+              <div className="flex justify-center items-center">
+                <input
+                  type="text"
+                  id="last_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  // placeholder={item.instagram}
+                  placeholder="www.instagram.com/juanperez"
+                  required
+                  disabled
+                />
+                <CiEdit
+                  className="bg-[#6254ff] text-1 text-white w-10 h-8 rounded-md cursor-pointer m-2"
+                  onClick={handleOpen}
+                />
               </div>
             </div>
-            <div>
-              <CiEdit
-                className="bg-[#6254ff] text-1 text-white w-10 h-8 rounded-md cursor-pointer"
-                onClick={handleOpen}
-              />
+            <div className="mt-5">
+              <label
+                htmlFor="last_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Phone
+              </label>
+              <div className="flex justify-center items-center">
+                <input
+                  type="text"
+                  id="last_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder={item.telephone}
+                  // placeholder="www.instagram.com/juanperez"
+                  required
+                  disabled
+                />
+                <CiEdit
+                  className="bg-[#6254ff] text-1 text-white w-10 h-8 rounded-md cursor-pointer m-2"
+                  onClick={handleOpen}
+                />
+              </div>
             </div>
           </div>
 
-          <div className=" flex items-center justify-between flex-col my-12">
-            <div className="mb-5 mt-5">
+          <div className=" flex items-center justify-between flex-col mt-5 mb-10">
+            <div className="">
               <label
                 for="countries"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -154,15 +201,15 @@ export default function NestedModal({ item }) {
                 id="countries"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option selected>Choose a country</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
+                {/* <option selected>Choose a country</option> */}
+                <option value="US">No Responde</option>
+                <option value="CA">Vendido</option>
+                <option value="FR">Rechazado</option>
+                <option value="DE">Sin Contactar</option>
               </select>
             </div>
-            <div className="">
-            <label
+            <div className="m-5">
+              <label
                 for="countries"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
@@ -172,17 +219,16 @@ export default function NestedModal({ item }) {
                 id="countries"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option selected>Choose a country</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
+                {/* <option selected>Choose a country</option> */}
+                <option value="US">Sin Dinero</option>
+                <option value="CA">Sin Interes</option>
+                <option value="FR">Otro Servicio</option>
+                {/* <option value="DE">Germany</option> */}
               </select>
-
             </div>
           </div>
 
-          <div>
+          <div className="">
             <ChildModal setOpen={setOpen} />
           </div>
         </Box>
--- client/src/components/Vendedores/Dashboard/Select/Select.jsx
+++ client/src/components/Vendedores/Dashboard/Select/Select.jsx
@@ -1,67 +1,40 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import FormHelperText from '@mui/material/FormHelperText';
 
-const style = {
-    'label + &': {
-      
-    },
-    '& .MuiInputBase-input': {
-      borderRadius: 4,
-      position: 'relative',
-      
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      
-      // Use the system font instead of the default Roboto font.
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
-    },
-  }
+
 
 export default function BasicSelect() {
-  const [age, setAge] = React.useState('');
+	const [age, setAge] = React.useState('');
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
+	const handleChange = (event) => {
+		setAge(event.target.value);
+	};
 
-  return (
-    <Box sx={{...style}}>
-      <FormControl fullWidth sx={{color:"white"}}>
-        <InputLabel id="demo-simple-select-label" sx={{color: "white"}}>Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-          sx={{color: "white"}}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  );
+	return (
+    <div>
+
+    <FormControl sx={{ m: 1, minWidth: 120, color: "white", borderRadius:55}}>
+      <Select
+        value={age}
+        onChange={handleChange}
+        displayEmpty
+        inputProps={{ 'aria-label': 'Without label' }}
+        sx={{ border:"1px solid white", '& .MuiSelect-icon': { color: '#fff' } }}
+      >
+        <MenuItem value=""  sx={{color: '#fff', border: "1px solid white" }}>
+          <em className='text-white'>None</em>
+        </MenuItem>
+        <MenuItem sx={{color: '#fff', border: "1px solid white" }} value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+      <FormHelperText>Without label</FormHelperText>
+    </FormControl>
+  </div>
+	);
 }
+
+
