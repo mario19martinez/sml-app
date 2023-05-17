61a8eaec3f91c7cbb94fff0433f62dcbdb9cbd3f
juanfacundotam <facutam@gmail.com>
2023-05-16 22:57:52 -0300

    Squashed commit of the following:
    commit d3575454e1b4ba5fff467826d48e60355bd353e1
    Merge: 892352b 8396a19
    Author: Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
    Date:   Tue May 16 22:56:08 2023 -0300
    
        Merge pull request #35 from juanfacundotam/develop-facu
    
        actualizacion de estilos vendedores Dashboard
    
    commit 8396a1953f6dba6097c07e8397a5b4ceb296b371
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 22:54:44 2023 -0300
    
        actualizacion de estilos vendedores Dashboard
    
        Se quito tremor y se reemplazo por las tags de tablas de html
    
    commit 892352b5c00ca9610ada1715e70c43e89dd9cf43
    Merge: e32d45a 5d7d3c0
    Author: Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
    Date:   Tue May 16 21:21:27 2023 -0300
    
        Merge pull request #34 from juanfacundotam/develop-facu
    
        Develop facu
    
    commit 5d7d3c0e1e98cf00608beed18e787aac2ebf0292
    Merge: 5f30c2d e32d45a
    Author: Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
    Date:   Tue May 16 21:21:11 2023 -0300
    
        Merge branch 'Smlappadm:develop-facu' into develop-facu
    
    commit 5f30c2da139a1dd1ae65fa681bc16888bfbb984c
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 21:20:32 2023 -0300
    
        modal updated
    
        Se actualizo el modal aun más
    
    commit c74680c8613bea0936f71c5dae4ea27417781064
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 19:00:24 2023 -0300
    
        actualizacion de modal
    
        Actualizando el modal para que quede intuitivo
    
    commit 738da811d0fb59fbbeecba153bd80a6d92dcd789
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 16:27:33 2023 -0300
    
        modal created
    
        Creación de modal y comienzo con la funcionalidad del mismo
    
    commit e32d45ac346421c7e641059b9d22a67795e3f8ec
    Merge: d91b6c4 d1f024c
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 14:24:08 2023 -0300
    
        Merge branch 'develop' into develop-facu
    
    commit 065e7d86f6e6f363697d2d3bb1b25e83c356b989
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 11:19:22 2023 -0300
    
        update dashboard vendedores bugs
    
    commit d91b6c4aeb7dc012790c9e170060bc92f558b772
    Merge: f309bab c9e4d54
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 10:57:14 2023 -0300
    
        Merge branch 'develop' into develop-facu
    
    commit f309babfc9c0a7d1f7fc5acdda075d028a8332fa
    Merge: 5c24de1 798548e
    Author: Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
    Date:   Tue May 16 10:54:54 2023 -0300
    
        Merge pull request #33 from juanfacundotam/develop-facu
    
        funcionalidad vendedores dashboard
    
    commit 798548e5751c1a2ac1b3463994d369828f82fc2a
    Author: juanfacundotam <facutam@gmail.com>
    Date:   Tue May 16 10:52:25 2023 -0300
    
        funcionalidad vendedores dashboard
    
        Agregando funcionalidad a vendedores dashboard

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -66,13 +66,15 @@ function ChildModal({ setOpen }) {
       >
         <Box sx={{ ...style, width: 500, backgroundColor: "#39394B", display:"flex", flexDirection:"column", justifyContent:"center", alignContent: "center" }}>
           <h2 id="child-modal-title" className="text-white text-center">
-            Are you sure?
+            Update the lead?
           </h2>
           {/* <p id="child-modal-description" className="text-white">
             ¿Seguro quieres guardar los cambios?
           </p> */}
           <div className="flex justify-around items-center m-5">
-          <Button
+          <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={handleClose}>No</button>
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleUpdate}>Yes</button>
+          {/* <Button
             onClick={handleClose}
             sx={{ color: "white", background: "#497d96" }}
           >
@@ -84,7 +86,7 @@ function ChildModal({ setOpen }) {
             className="text-center bg-black"
           >
             Yes
-          </Button>
+          </Button> */}
           </div>
         </Box>
       </Modal>
