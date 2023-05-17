6dfe56982c44e860ffd87bd2436a8a5c9a4c0803
Jonavoe <voeffray.jonathan@gmail.com>
2023-05-17 18:05:52 -0300

    cambios a employees

--- client/src/components/Lideres/Employees/TableEmployees/MaterialUi/BasicSelect.jsx
+++ client/src/components/Lideres/Employees/TableEmployees/MaterialUi/BasicSelect.jsx
@@ -1,58 +1,57 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { styled } from '@mui/material/styles';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { styled } from "@mui/material/styles";
 
 const StyledMenuItem = styled(MenuItem)(() => ({
-	'&:hover': {
-		backgroundColor: '#39394b'
-	},
-	'&.Mui-selected': {
-	},
+  "&:hover": {
+    backgroundColor: "#39394b",
+  },
+  "&.Mui-selected": {},
 }));
 
 const StyledInputLabel = styled(InputLabel)(() => ({
-	color: '#fff',
+  color: "#fff",
 }));
 
 const StyledSelect = styled(Select)(() => ({
-	'& .MuiSelect-icon': { color: '#fff' },
-	'& .MuiInputBase-input': { color: '#fff' },
+  "& .MuiSelect-icon": { color: "#fff" },
+  "& .MuiInputBase-input": { color: "#fff" },
 }));
 
 const StyledFormControl = styled(FormControl)(() => ({
-	'& .MuiInputLabel-formControl': {
-		color: '#fff',
-	},
+  "& .MuiInputLabel-formControl": {
+    color: "#fff",
+  },
 }));
 
-export default function BasicSelect({employees, setEmployees}) {
+export default function BasicSelect({ employees, setEmployees }) {
+  const handleChange = (event) => {
+    setEmployees(event.target.value);
+  };
 
-	const handleChange = (event) => {
-		setEmployees(event.target.value);
-	};
-
-	return (
-		<Box sx={{ width: '50%' }}>
-			<StyledFormControl fullWidth>
-				<StyledInputLabel id='demo-simple-select-label'>
-					Employees
-				</StyledInputLabel>
-				<StyledSelect
-					labelId='demo-simple-select-label'
-					id='demo-simple-select'
-					value={employees}
-					label='Employees'
-					onChange={handleChange}>
-					<StyledMenuItem value={'c-level'}>C-Level</StyledMenuItem>
-					<StyledMenuItem value={'leader'}>Leader</StyledMenuItem>
-					<StyledMenuItem value={'corredor'}>Corredor</StyledMenuItem>
-					<StyledMenuItem value={'vendedor'}>Vendedor</StyledMenuItem>
-				</StyledSelect>
-			</StyledFormControl>
-		</Box>
-	);
+  return (
+    <Box sx={{ width: "50%" }}>
+      <StyledFormControl fullWidth>
+        <StyledInputLabel id="demo-simple-select-label">
+          Employees
+        </StyledInputLabel>
+        <StyledSelect
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={employees}
+          label="Employees"
+          onChange={handleChange}
+        >
+          <StyledMenuItem value={"c-level"}>C-Level</StyledMenuItem>
+          <StyledMenuItem value={"leader"}>Leader</StyledMenuItem>
+          <StyledMenuItem value={"corredor"}>Corredor</StyledMenuItem>
+          <StyledMenuItem value={"vendedor"}>Vendedor</StyledMenuItem>
+        </StyledSelect>
+      </StyledFormControl>
+    </Box>
+  );
 }
--- client/src/components/Lideres/Employees/TableEmployees/TableEmployees.jsx
+++ client/src/components/Lideres/Employees/TableEmployees/TableEmployees.jsx
@@ -1,126 +1,124 @@
-import { useState, useEffect } from 'react';
-import style from './TableEmployees.module.css';
-import PaginationOutlined from '../../../pagination/PaginationOutlined';
+import { useState, useEffect } from "react";
+import style from "./TableEmployees.module.css";
+import PaginationOutlined from "../../../pagination/PaginationOutlined";
 import {
-	Card,
-	Table,
-	TableHead,
-	TableRow,
-	TableHeaderCell,
-	TableBody,
-	TableCell,
-	Text,
-	Title,
-	Badge,
-} from '@tremor/react';
-import { HiOutlineDotsHorizontal } from 'react-icons/hi';
-import { CiMail } from 'react-icons/ci';
-import { getAllCorredores, getAllVendedores } from '../../../../redux/actions';
-import { useDispatch, useSelector } from 'react-redux';
-import NestedModal from './MaterialUi/NestedModal';
+  Card,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Text,
+  Title,
+  Badge,
+} from "@tremor/react";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { CiMail } from "react-icons/ci";
+import { getAllCorredores, getAllVendedores } from "../../../../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import NestedModal from "./MaterialUi/NestedModal";
 //
 export const TableEmployees = () => {
-	const { corredores, vendedores } = useSelector((state) => state);
-	const dispatch = useDispatch();
+  const { corredores, vendedores } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getAllCorredores());
-		dispatch(getAllVendedores());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(getAllCorredores());
+    dispatch(getAllVendedores());
+  }, [dispatch]);
 
-	let employees = corredores.concat(vendedores);
+  let employees = corredores.concat(vendedores);
 
-	console.log(employees);
+  console.log(employees);
 
-	const [pageStyle, setPageStyle] = useState(1);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [cardXPage, setCardXpage] = useState(10);
-	const indexLastCard = currentPage * cardXPage;
-	const indexFirstCard = indexLastCard - cardXPage;
-	const currentCard = employees.slice(indexFirstCard, indexLastCard);
+  const [pageStyle, setPageStyle] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardXPage, setCardXpage] = useState(10);
+  const indexLastCard = currentPage * cardXPage;
+  const indexFirstCard = indexLastCard - cardXPage;
+  const currentCard = employees.slice(indexFirstCard, indexLastCard);
 
-	const pages = (pageNumber) => {
-		setCurrentPage(pageNumber);
-	};
+  const pages = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-	return (
-		<>
-			<div className=' flex flex-col justify-start items-center w-full h-screen'>
-				<Card className='bg-[#222131] rounded-none w-full h-screen p-5'>
-					<div className='flex justify-between items-center'>
-						<Title className='font-bold text-[#e2e2e2] text-lg ml-4'>
-							Employees
-						</Title>
-						<NestedModal />
-					</div>
-					<Table className=''>
-						<TableHead className={style.tableHead}>
-							<TableRow className={style.tableRow}>
-								<TableHeaderCell className='text-start'>Name</TableHeaderCell>
-								<TableHeaderCell className='text-start'>Email</TableHeaderCell>
-								<TableHeaderCell className='text-start'>
-									Phone Number
-								</TableHeaderCell>
-								<TableHeaderCell className='text-start'>
-									Position
-								</TableHeaderCell>
-								<TableHeaderCell className='text-start'>
-									<h1></h1>
-								</TableHeaderCell>
-							</TableRow>
-						</TableHead>
+  return (
+    <>
+      <div className=" flex flex-col justify-start items-center w-full h-screen">
+        <Card className="bg-[#222131] rounded-none w-full h-screen p-5">
+          <div className="flex justify-between items-center">
+            <Title className="font-bold text-[#e2e2e2] text-lg ml-4">
+              Employees
+            </Title>
+            <NestedModal />
+          </div>
+          <Table className="">
+            <TableHead className={style.tableHead}>
+              <TableRow className={style.tableRow}>
+                <TableHeaderCell className="text-start">Name</TableHeaderCell>
+                <TableHeaderCell className="text-start">Email</TableHeaderCell>
+                <TableHeaderCell className="text-start">
+                  Phone Number
+                </TableHeaderCell>
+                <TableHeaderCell className="text-start">
+                  Position
+                </TableHeaderCell>
+                <TableHeaderCell className="text-start">
+                  <h1></h1>
+                </TableHeaderCell>
+              </TableRow>
+            </TableHead>
 
-						<TableBody className={style.tableBody}>
-							{currentCard.map((item, index) => (
-								<TableRow
-									key={index}
-									className={style.tableCards}>
-									<TableCell className='flex justify-start items-center p-0'>
-										<img
-											className='w-8 ml-2 mr-4 rounded-full'
-											src={item.photo}
-											alt='avatar image'
-										/>
-										<Text className='text-start'>{item.name}</Text>
-									</TableCell>
-									<TableCell className='flex justify-start items-center p-0'>
-										<CiMail className={style.icon} />
-										<Text className='text-start'>{item.email}</Text>
-									</TableCell>
-									<TableCell className='p-0 mx-3'>
-										<Text className='text-start'>{item.contactNumber}</Text>
-									</TableCell>
-									<TableCell className='p-0'>
-										{item.classifications === 'Corredor' ? (
-											<Text className='bg-[#29c08b]  text-[#050505] text-center p-1 w-20 rounded-lg'>
-												{item.classifications}
-											</Text>
-										) : (
-											<Text className='bg-[#ca4f8d] text-[#f1f1f1] text-center p-1 px-2 w-20 rounded-lg'>
-												{item.classifications}
-											</Text>
-										)}
-									</TableCell>
-									<TableCell className='p-0'>
-										<a href=''>
-											<HiOutlineDotsHorizontal className='text-18' />
-										</a>
-									</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</Card>
-				<div className=' mb-6'>
-					<PaginationOutlined
-						pageStyle={pageStyle}
-						setPageStyle={setPageStyle}
-						cardXPage={cardXPage}
-						data={employees}
-						pages={pages}
-					/>
-				</div>
-			</div>
-		</>
-	);
+            <TableBody className={style.tableBody}>
+              {currentCard.map((item, index) => (
+                <TableRow key={index} className={style.tableCards}>
+                  <TableCell className="flex justify-start items-center p-0">
+                    <img
+                      className="w-8 ml-2 mr-4 rounded-full"
+                      src={item.photo}
+                      alt="avatar image"
+                    />
+                    <Text className="text-start">{item.name}</Text>
+                  </TableCell>
+                  <TableCell className="flex justify-start items-center p-0">
+                    <CiMail className={style.icon} />
+                    <Text className="text-start">{item.email}</Text>
+                  </TableCell>
+                  <TableCell className="p-0 mx-3">
+                    <Text className="text-start">{item.contactNumber}</Text>
+                  </TableCell>
+                  <TableCell className="p-0">
+                    {item.classifications === "Corredor" ? (
+                      <Text className="bg-[#29c08b]  text-[#050505] text-center p-1 w-20 rounded-lg">
+                        {item.classifications}
+                      </Text>
+                    ) : (
+                      <Text className="bg-[#ca4f8d] text-[#f1f1f1] text-center p-1 px-2 w-20 rounded-lg">
+                        {item.classifications}
+                      </Text>
+                    )}
+                  </TableCell>
+                  <TableCell className="p-0">
+                    <a href="">
+                      <HiOutlineDotsHorizontal className="text-18" />
+                    </a>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Card>
+        <div className=" mb-6">
+          <PaginationOutlined
+            pageStyle={pageStyle}
+            setPageStyle={setPageStyle}
+            cardXPage={cardXPage}
+            data={employees}
+            pages={pages}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
